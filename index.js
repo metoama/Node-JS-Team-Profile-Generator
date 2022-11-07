759130f0109af43c64affb373b7e4b786264a474
CAGE1899 <metoama@gmail.com>
2022-11-07 05:59:44 -0500

    working on index.js questions for app

--- index.js
+++ index.js
@@ -10,7 +10,34 @@ const distPath = path.join(dist_dir, "team.html");
 const teamMembers = [];
 
 const promptManger = () => {
-    return inquirer.prompt()
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is the team manager's name?"
+        },
+
+        {
+            tytype: 'input',
+            name: 'id',
+            message: "What is the team manager's id?"
+
+        },
+
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is the team manager's email"
+        },
+        
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: "What is the team manager's office number?"
+        }
+    ]
+
+    )
 }
 
 
