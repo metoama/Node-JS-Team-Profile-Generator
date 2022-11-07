0c4ee9db3d2b7a3e2e6fdde55604ee57db482891
CAGE1899 <metoama@gmail.com>
2022-11-07 05:24:36 -0500

    fixed employee test, now officially passing

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -5,7 +5,7 @@ const employee = new Employee('Arinze', '1234567', 'metoama@gmail.com');
 test('test contrsuctor values for employee', () => {
     expect(employee.name).toBe('Arinze');
     expect(employee.id).toBe('1234567');
-    expect(employee.email).toBe('metoama@gmail.com');
+    expect(employee.email).toBe("metoama@gmail.com");
 });
 
 test('test if we get the name from getName() method', () => {
@@ -17,7 +17,7 @@ test('test if getId() method will return id',  () => {
 });
 
 test('test if getEmail() method will return email', () => {
-    expect(employee.getEmail()).toBe('metoama@gmail.com');
+    expect(employee.getEmail()).toBe("metoama@gmail.com");
 });
   
 test('test if getRole method will return role', () => {
---
+++ tests/engineer.test.js
@@ -0,0 +1,24 @@
+const Employee = require('../lib/employee');
+const employee = new Employee('Arinze', '1234567', 'metoama@gmail.com');
+
+test('test contrsuctor values for employee', () => {
+    expect(employee.name).toBe('Arinze');
+    expect(employee.id).toBe('1234567');
+    expect(employee.email).toBe('metoama@gmail.com');
+});
+
+test('test if we get the name from getName() method', () => {
+    expect(employee.getName()).toBe('Arinze');
+});
+
+test('test if getId() method will return id',  () => {
+    expect(employee.getId()).toBe('1234567');
+});
+
+test('test if getEmail() method will return email', () => {
+    expect(employee.getEmail()).toBe('metoama@gmail.com');
+});
+  
+test('test if getRole method will return role', () => {
+    expect(employee.getRole()).toBe('employee');
+});
