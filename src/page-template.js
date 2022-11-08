cbe6099b9f7aa460b05799b028b409c4da8498a3
CAGE1899 <metoama@gmail.com>
2022-11-08 07:21:15 -0500

    pushing up completed html, have issue with output and app function

--- src/page-template.js
+++ src/page-template.js
@@ -15,6 +15,76 @@ const generateTeam = (team) => {
             <ul class="list-group list-group-flush">
             <li class="list-group-item">Id: ${manager.id}</li>
             <li class="list-group-item">Email: <span id="email"><a href="mailto:${manager.email}">${manager.email}</a></span></li>
-            <li class="list-group-item">Office Number: ${manager.officeNumber}</li>`
+            <li class="list-group-item">Office Number: ${manager.officeNumber}</li>
+            </ul>
+        </div>`;
+        html.push(managerHtml);
+    } //engineer
+    const generateEngineer = engineer => {
+        console.log(engineer);
+        let engineerHtml = `
+        <div class="card" style="width 15rem;">
+            <div class="card-header">
+            ${engineer.name} <br/>
+            <i class="fas fa-mug-hot></i>Manager</div>
+            <ul class="list-group list-group-flush">
+            <li class="list-group-item">Id: ${engineer.id}</li>
+            <li class="list-group-item">Email: <span id="email"><a href="mailto:${engineer.email}">${engineer.email}</a></span></li>
+            <li class="list-group-item">Github: <a target="_blank" href="https://github.com/${engineer.github}">${engineer.github}</a></li>
+            </ul>
+        </div>`;
+        html.push(engineerHtml);
+}
+const generateIntern = intern => {
+    console.log(intern);
+    let internHtml = `
+    <div class="card" style="width 15rem;">
+        <div class="card-header">
+        ${intern.name} <br/>
+        <i class="fas fa-mug-hot></i>Manager</div>
+        <ul class="list-group list-group-flush">
+        <li class="list-group-item">Id: ${intern.id}</li>
+        <li class="list-group-item">Email: <span id="email"><a href="mailto:${intern.email}">${intern.email}</a></span></li>
+        <li class="list-group-item">School: ${intern.School}</li>
+        </ul>
+    </div>`;
+    html.push(internHtml)};
+
+for (let i = 0; i < team.length; i++) {
+    if (team[i].getRole() === "Manager") {
+        generateManager(team[i]);
     }
-};
+    if (team[i].getRole() === "Engineer") {
+        generateEngineer(team[i]);
+    }
+    if (team[i].getRole() === "Intern") {
+        generateIntern(team[i]);
+    };
+}
+
+return html.join('');
+}
+
+module.exports = team => {
+
+    return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"/>
+        <link rel="stylesheet" href="../dist/style.css"/>  
+        <title>Team Profile Generator</title>
+    </head>
+    <body>
+        <header>
+        <h1>The Team</h1>
+        </header>
+    
+        <main> ${generateTeam(team)}</main>
+    </body>
+    </html>;`
+    
+    
+}
